cc1dc7aed5e89f2eb15f76fe6d89fa743f00e85c
Mattt <mattt@me.com>
2018-10-04 15:14:50 -0700

    Secure links to cross-origin destinations

--- _plugins/markdown-converter.rb
+++ _plugins/markdown-converter.rb
@@ -19,8 +19,9 @@ module Jekyll
           doc = Nokogiri::HTML::DocumentFragment.parse(html)
 
           remove_proprietary_attributes!(doc)
-          transform_code_symbols!(doc)
+          secure_links_to_cross_origin_destinations!(doc)
           transform_apple_trademarks!(doc)
+          transform_code_symbols!(doc)
 
           doc.to_html
         end
@@ -36,16 +37,26 @@ module Jekyll
         def transform_code_symbols!(doc)
           doc.css('p > code, li > code, td > code').each do |code|
             code.remove_attribute('class')
-            code.inner_html = code.inner_html.gsub(/(?:([a-z])([A-Z]+))/, "\\1<wbr/>\\2")
+            code.inner_html = code.inner_html.gsub(/(?:([a-z])([A-Z]+))/, '\\1<wbr/>\\2')
           end
         end
 
         def transform_apple_trademarks!(doc)
           doc.css('p, li, td').each do |p|
-            p.inner_html = p.inner_html.gsub(/iPhone X([SR])/, "iPhone X<small>\\1</small>")
+            p.inner_html = p.inner_html.gsub(/iPhone X([SR])/, 'iPhone X<small>\\1</small>')
+          end
+        end
+
+        def secure_links_to_cross_origin_destinations!(doc)
+          doc.css('a[href]').each do |a|
+            href = a.attr('href')
+            next if href.match?(/^\/|#{ENV['DOMAIN']}/)
+
+            a['rel'] = 'noopener'
           end
         end
       end
     end
   end
 end
+
