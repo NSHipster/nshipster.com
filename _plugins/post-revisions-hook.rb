a56ceecfb326bd4439a64cbfd759bcc66e67a177
Mattt <mattt@me.com>
2018-10-08 11:40:08 -0700

    Fix ordering of new and updated articles

--- _plugins/post-revisions-hook.rb
+++ _plugins/post-revisions-hook.rb
@@ -4,9 +4,10 @@ require 'pp'
 Jekyll::Hooks.register :posts, :pre_render do |post|
     if post.data['revisions']
         raise unless last_revised_on = Time.parse(post.data['revisions'].keys.max)
+        post.data['updated_on'] = last_revised_on
         post.data['last_revised_on'] = last_revised_on
         post.data['revision_description'] = post.data['revisions'][last_revised_on]
     else
-        post.data['last_revised_on'] = post.date
+        post.data['updated_on'] = post.date
     end
 end
