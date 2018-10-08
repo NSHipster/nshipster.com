aa32432e69830ebdac55ccd054144faf198066b8
Mattt <mattt@me.com>
2018-10-08 11:53:37 -0700

    Fix revision description for updated articles

--- _plugins/post-revisions-hook.rb
+++ _plugins/post-revisions-hook.rb
@@ -3,10 +3,11 @@ require 'pp'
 
 Jekyll::Hooks.register :posts, :pre_render do |post|
     if post.data['revisions']
-        raise unless last_revised_on = Time.parse(post.data['revisions'].keys.max)
-        post.data['updated_on'] = last_revised_on
+        latest_revision = post.data['revisions'].keys.max_by { |k| Time.parse(k) }
+        raise unless latest_revision and last_revised_on = Time.parse(latest_revision)
+        post.data['revision_description'] = post.data['revisions'][latest_revision]
         post.data['last_revised_on'] = last_revised_on
-        post.data['revision_description'] = post.data['revisions'][last_revised_on]
+        post.data['updated_on'] = last_revised_on
     else
         post.data['updated_on'] = post.date
     end
