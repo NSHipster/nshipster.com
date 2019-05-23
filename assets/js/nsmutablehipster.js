74769fc075e7711c14aa2fd8a823cbafac877cd3
Mattt <mattt@me.com>
2019-05-23 11:00:45 -0700

    Conditionalize Typed on existence of .tagline

--- assets/js/nsmutablehipster.js
+++ assets/js/nsmutablehipster.js
@@ -6,17 +6,19 @@
     element.contentEditable = false;
   });
 
-  new Typed(".tagline strong", {
-    strings: ["NSHipster", "NSMutableHipster"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    startDelay: 1000,
-    onComplete: () => {
-      document.querySelector(".typed-cursor").remove();
-    }
-  });
+  if (document.querySelector(".tagline")) {
+    new Typed(".tagline strong", {
+      strings: ["NSHipster", "NSMutableHipster"],
+      typeSpeed: 100,
+      backSpeed: 100,
+      startDelay: 1000,
+      onComplete: () => {
+        document.querySelector(".typed-cursor").remove();
+      }
+    });
 
-  document
-    .querySelector(".typed-cursor")
-    .setAttribute("style", "position: absolute;");
+    document
+      .querySelector(".typed-cursor")
+      .setAttribute("style", "position: absolute;");
+  }
 })();
