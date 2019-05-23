98ac429909e1f29f90216fcd2c2d63f11e3c0560
Mattt <mattt@me.com>
2019-05-23 10:55:38 -0700

    Add typing effect to NSMutableHipster

--- assets/js/nsmutablehipster.js
+++ assets/js/nsmutablehipster.js
@@ -1,6 +1,22 @@
+//= require vendor/typed.min.js
+
 (function() {
   document.body.contentEditable = true;
   document.querySelectorAll("a").forEach(element => {
     element.contentEditable = false;
   });
+
+  new Typed(".tagline strong", {
+    strings: ["NSHipster", "NSMutableHipster"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    startDelay: 1000,
+    onComplete: () => {
+      document.querySelector(".typed-cursor").remove();
+    }
+  });
+
+  document
+    .querySelector(".typed-cursor")
+    .setAttribute("style", "position: absolute;");
 })();
