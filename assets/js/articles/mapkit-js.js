16cc82be54c580f19a3442b12576575f9520549a
Mattt <mattt@me.com>
2019-05-23 11:11:50 -0700

    Wrap script in self-executing function

--- assets/js/articles/mapkit-js.js
+++ assets/js/articles/mapkit-js.js
@@ -1,27 +1,30 @@
-mapkit.init({
-  authorizationCallback: done => {
-    done(
-      "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjRUOTJZWlNXR00ifQ.eyJleHAiOjI0MTYzMjE2MDAuMjc2NjE2MSwiaXNzIjoiOUpXVkFEUjNSUSIsIm9yaWdpbiI6Imh0dHBzOlwvXC9uc2hpcHN0ZXIuY29tIiwiaWF0IjoxNTUyMzIxNjAwLjI3NjYxNzF9.DRsKD-6Y9MMVId46PnV9DK-YiobqGu0qCMbK1fYjglWOVnuJkpm4UGDdYLB0T4xJfStPlrSI1Pwbzsjn_i0Qww"
-    );
-  }
-});
+(function() {
+  mapkit.init({
+    authorizationCallback: done => {
+      done(
+        "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjRUOTJZWlNXR00ifQ.eyJleHAiOjI0MTYzMjE2MDAuMjc2NjE2MSwiaXNzIjoiOUpXVkFEUjNSUSIsIm9yaWdpbiI6Imh0dHBzOlwvXC9uc2hpcHN0ZXIuY29tIiwiaWF0IjoxNTUyMzIxNjAwLjI3NjYxNzF9.DRsKD-6Y9MMVId46PnV9DK-YiobqGu0qCMbK1fYjglWOVnuJkpm4UGDdYLB0T4xJfStPlrSI1Pwbzsjn_i0Qww"
+      );
+    }
+  });
 
-const center = new mapkit.Coordinate(37.3327, -122.0053),
-  span = new mapkit.CoordinateSpan(0.0125, 0.0125),
-  region = new mapkit.CoordinateRegion(center, span);
+  const center = new mapkit.Coordinate(37.3327, -122.0053),
+    span = new mapkit.CoordinateSpan(0.0125, 0.0125),
+    region = new mapkit.CoordinateRegion(center, span);
 
-let map = new mapkit.Map("map", {
-  region: region,
-  showsCompass: mapkit.FeatureVisibility.Hidden,
-  showsZoomControl: false,
-  showsMapTypeControl: false
-});
+  let map = new mapkit.Map("map", {
+    region: region,
+    showsCompass: mapkit.FeatureVisibility.Hidden,
+    showsZoomControl: false,
+    showsMapTypeControl: false
+  });
 
-const annotation = new mapkit.MarkerAnnotation(center, {
-  title: "Apple Park Visitor Center",
-  subtitle: "10600 North Tantau Avenue, Cupertino, CA 95014",
-  glyphText: "",
-  color: "#8e8e93",
-  displayPriority: 1000
-});
-map.addAnnotation(annotation);
+  const annotation = new mapkit.MarkerAnnotation(center, {
+    title: "Apple Park Visitor Center",
+    subtitle: "10600 North Tantau Avenue, Cupertino, CA 95014",
+    glyphText: "",
+    color: "#8e8e93",
+    displayPriority: 1000
+  });
+
+  map.addAnnotation(annotation);
+})();
