c37047387d9ee630551a84c8565b81a517ec1f75
Mattt <mattt@me.com>
2019-10-24 11:13:56 -0700

    Fix search function

--- _functions/search/search.js
+++ _functions/search/search.js
@@ -1,33 +1,32 @@
-import lunr from "lunr";
+const fs = require("fs");
+const lunr = require("lunr");
 
-export const handler = (event, context, callback) => {
-  (async () => {
-    const query = event.queryStringParameters.q;
+const json = JSON.parse(fs.readFileSync(require.resolve("./index.json")));
+var index = lunr(function() {
+  this.ref("url");
+  this.field("title", { boost: 10 });
+  this.field("category");
+  this.field("excerpt");
 
-    if (!query || query === "") {
-      callback(null, {
-        statusCode: 400
-      });
-    } else {
-      const json = fs.readFileSync(require.resolve("./index.json"));
+  json.forEach(function(doc) {
+    console.log(doc);
+    this.add(doc);
+  }, this);
+});
 
-      var index = lunr(function() {
-        this.ref("url");
-        this.field("title", { boost: 10 });
-        this.field("category");
-        this.field("excerpt");
+exports.handler = function(event, context, callback) {
+  const query = event.queryStringParameters.q;
 
-        json.forEach(function(doc) {
-          this.add(doc);
-        }, this);
-      });
-
-      const results = index.search(query);
-
-      callback(null, {
-        statusCode: 200,
-        body: JSON.stringify(results)
-      });
-    }
-  })();
+  if (!query || query === "") {
+    callback(null, {
+      statusCode: 400,
+      body: JSON.stringify({ message: "Invalid query" })
+    });
+  } else {
+    const results = index.search(query);
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(results)
+    });
+  }
 };
