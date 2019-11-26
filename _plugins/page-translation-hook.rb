a85ef50c02f4ff0baf2ff6d1d320f6abdf7311b5
Mattt <mattt@me.com>
2019-11-26 06:19:20 -0800

    Refactor generation of alternate links in head

--- _plugins/page-translation-hook.rb
+++ _plugins/page-translation-hook.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 Jekyll::Hooks.register :pages, :pre_render do |page|
-  page.data['translations'] = page.site.config['languages'] if page.index?
+  page.data['translations'] = page.site.config['domains'].keys if page.index?
 end
