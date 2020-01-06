745d46976b34598750ca86594800a2e4a83e75b6
Mattt <mattt@me.com>
2020-01-06 07:21:43 -1000

    Allow both YAML date literals and timestamp strings

--- _plugins/post-revisions-hook.rb
+++ _plugins/post-revisions-hook.rb
@@ -4,8 +4,8 @@ require 'date'
 
 Jekyll::Hooks.register :posts, :pre_render do |post|
   if post.data['revisions']
-    latest_revision = post.data['revisions'].keys.max_by { |k| Time.parse(k) }
-    raise unless latest_revision && (last_revised_on = Time.parse(latest_revision))
+    latest_revision = post.data['revisions'].keys.max
+    raise unless latest_revision && (last_revised_on = Time.parse("#{latest_revision}"))
 
     post.data['revision_description'] = post.data['revisions'][latest_revision]
     post.data['last_revised_on'] = last_revised_on
