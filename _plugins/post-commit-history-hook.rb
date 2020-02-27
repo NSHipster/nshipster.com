d98b64ed6ff112d2c95e40597d4cb598717da67c
Mattt <mattt@me.com>
2020-02-27 08:36:22 -0800

    Add warning when configuration doesn't specify language

--- _plugins/post-commit-history-hook.rb
+++ _plugins/post-commit-history-hook.rb
@@ -3,20 +3,22 @@
 require 'uri'
 
 Jekyll::Hooks.register :posts, :pre_render do |post|
-    language = post.site.config['lang']
+  language = post.site.config['lang']
 
-    repository_name = case language
-                      when /^en/ then "articles"
-                      when /^es/ then "articles-es"
-                      when /^fr/ then "articles-fr"
-                      when /^ko/ then "articles-ko"
-                      when /^zh-Hans/ then "articles-zh-Hans"
-                      when /^ru/ then "articles-ru"
-                      else
-                          raise "Unknown language: #{language}"
-                      end
+  repository_name = case language
+                    when /^en/ then 'articles'
+                    when /^es/ then 'articles-es'
+                    when /^fr/ then 'articles-fr'
+                    when /^ko/ then 'articles-ko'
+                    when /^zh-Hans/ then 'articles-zh-Hans'
+                    when /^ru/ then 'articles-ru'
+                    when nil
+                      raise 'Unspecified language in site configuration'
+                    else
+                      raise "Unknown language: #{language}"
+                    end
 
-    filename = File.basename(post.path)
+  filename = File.basename(post.path)
 
-    post.data['commit_history_url'] = "https://github.com/nshipster/#{repository_name}/commits/master/#{filename}"
+  post.data['commit_history_url'] = "https://github.com/nshipster/#{repository_name}/commits/master/#{filename}"
 end
